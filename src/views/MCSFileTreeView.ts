c8d7f11b46f72526abda0582f508e7f88aab3271
liruohrh <2372221537@qq.com>
2025-02-06 18:57:12 +0800

    style: 格式化

--- src/views/MCSFileTreeView.ts
+++ src/views/MCSFileTreeView.ts
@@ -1,33 +1,33 @@
 import * as vscode from "vscode";
 
 export class MCSFileTreeView
-  implements vscode.TreeDataProvider<vscode.TreeItem>
+    implements vscode.TreeDataProvider<vscode.TreeItem>
 {
-  private _onDidChangeTreeData = new vscode.EventEmitter<void>();
-  readonly onDidChangeTreeData = this._onDidChangeTreeData.event;
+    private _onDidChangeTreeData = new vscode.EventEmitter<void>();
+    readonly onDidChangeTreeData = this._onDidChangeTreeData.event;
 
-  refresh(): void {
-    this._onDidChangeTreeData.fire();
-  }
-
-  getTreeItem(element: vscode.TreeItem): vscode.TreeItem {
-    return element;
-  }
-
-  async getChildren(element?: vscode.TreeItem): Promise<vscode.TreeItem[]> {
-    if (!element) {
-      return this.getInstanceItems();
+    refresh(): void {
+        this._onDidChangeTreeData.fire();
     }
-    return this.getFileItems(element);
-  }
 
-  private async getInstanceItems(): Promise<vscode.TreeItem[]> {
-    return [new vscode.TreeItem("实例1")];
-  }
+    getTreeItem(element: vscode.TreeItem): vscode.TreeItem {
+        return element;
+    }
 
-  private async getFileItems(
-    parent: vscode.TreeItem
-  ): Promise<vscode.TreeItem[]> {
-    return [new vscode.TreeItem("文件示例")];
-  }
+    async getChildren(element?: vscode.TreeItem): Promise<vscode.TreeItem[]> {
+        if (!element) {
+            return this.getInstanceItems();
+        }
+        return this.getFileItems(element);
+    }
+
+    private async getInstanceItems(): Promise<vscode.TreeItem[]> {
+        return [new vscode.TreeItem("实例1")];
+    }
+
+    private async getFileItems(
+        parent: vscode.TreeItem
+    ): Promise<vscode.TreeItem[]> {
+        return [new vscode.TreeItem("文件示例")];
+    }
 }

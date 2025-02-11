7760094ad393907c6b0bfd7bdde6b5a1ae2033f5
liruohrh <2372221537@qq.com>
2025-02-11 16:04:38 +0800

    chore: 暂时不用虚拟文件

--- src/filesystem/MCSTextDocument.ts
+++ src/filesystem/MCSTextDocument.ts
@@ -1,14 +1,12 @@
-import * as vscode from "vscode";
-import { GlobalVar } from "@/utils/global";
+// import * as vscode from "vscode";
+// import { GlobalVar } from "@/utils/global";
 
-export class MCSTextDocumentContentProvider
-    implements vscode.TextDocumentContentProvider
-{
-    private _onDidChange = new vscode.EventEmitter<vscode.Uri>();
-    onDidChange = this._onDidChange.event;
-
-    async provideTextDocumentContent(uri: vscode.Uri): Promise<string> {
-        const content = await GlobalVar.mcsService.getFileContent(uri.path);
-        return content;
-    }
+export class MCSTextDocumentContentProvider {
+    // implements vscode.TextDocumentContentProvider
+    // private _onDidChange = new vscode.EventEmitter<vscode.Uri>();
+    // onDidChange = this._onDidChange.event;
+    // async provideTextDocumentContent(uri: vscode.Uri): Promise<string> {
+    //     const content = await GlobalVar.mcsService.getFileContent(uri.path);
+    //     return content;
+    // }
 }

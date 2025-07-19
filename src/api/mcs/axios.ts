0ae5d2159f7df689a529048e4794427081c615fe
liruohrh <2372221537@qq.com>
2025-07-20 01:54:53 +0800

    fix: axios 不默认fetch

--- src/api/mcs/axios.ts
+++ src/api/mcs/axios.ts
@@ -3,7 +3,6 @@ import axiosG from "axios";
 import https from "https";
 
 const axios = axiosG.create({
-    adapter: "fetch",
     httpsAgent: new https.Agent({ rejectUnauthorized: Config.sslVerify }),
 });
 export const axiosMcs = axios;
